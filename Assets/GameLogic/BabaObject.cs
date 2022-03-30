742b4c4e3996fc6e2fa303dcca5ff61f99ea98c2
moonman13 <98766172+moonman13@users.noreply.github.com>
2022-03-30 12:29:36 -0400

    Update BabaObject.cs

--- Assets/GameLogic/BabaObject.cs
+++ Assets/GameLogic/BabaObject.cs
@@ -4,9 +4,58 @@ using UnityEngine;
 
 public class BabaObject : MonoBehaviour
 {
-    public ObjectType type;
 
+    //General
+    public ObjectType myType;
 
+    //Obstacle
+    private Collider2D myCol;
+
+    private void Start()
+    {
+
+    }
+    private void Update()
+    {
+        PlayerUpdate();
+    }
+    private void PlayerUpdate()
+    {
+        if (myType.Equals(ObjectType.Player))
+        {
+            // Input
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // OK so I'm not sure how exactly we're handling movement on the grid
+        // Maybe we aren't using collisions, who knows
+        // I'm going to wait until we've nailed down the movement to start messing with these
+
+        if (myType.Equals(ObjectType.Killer))
+        {
+            if (collision.collider.gameObject.CompareTag("Player"))
+            {
+                //Kill the player;
+            }
+        }
+        if (myType.Equals(ObjectType.Win))
+        {
+            if (collision.collider.gameObject.CompareTag("Player"))
+            {
+                // Next level
+            }
+        }
+        if (myType.Equals(ObjectType.Pushable))
+        {
+            if (collision.collider.gameObject.CompareTag("Player"))
+            {
+                // Move
+            }
+        }
+
+    }
 }
 
 public enum ObjectType
@@ -14,4 +63,7 @@ public enum ObjectType
     Player,
     Obstacle,
     Killer,
+    Win,
+    Pushable,
 }
+
