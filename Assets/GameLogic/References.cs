a8c2307bc7bc2160bdd1e0a28268ebfa005ce119
moonman13 <98766172+moonman13@users.noreply.github.com>
2022-04-25 13:42:09 -0400

    Jack - BabaSprites

--- Assets/GameLogic/References.cs
+++ Assets/GameLogic/References.cs
@@ -5,5 +5,9 @@ using UnityEngine;
 using SKCell;
 public class References : MonoSingleton<References>
 {
-    public Sprite a;
+    public Sprite Baba;
+    public Sprite Wall;
+    public Sprite Rock;
+    public Sprite Flag;
+    public Sprite Death;
 }
